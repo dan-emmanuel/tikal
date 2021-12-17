2ccf67ac63fe4946e5a68b6d27dfa398af640911
dan-emmanuel <d.e.azoulay@gmail.com>
2021-12-17 05:42:43 +0200

    update

--- src/components/Part2/Part2.js
+++ src/components/Part2/Part2.js
@@ -2,15 +2,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import { getPlanets } from "../../requester/part2"
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 let Part2 = (props) => {
-    const [planet, setPlanet] = useState("")
+    const [planets, setPlanet] = useState([])
     useEffect(() => {
         (async () => {
-            let planets = await getPlanets()
-            console.log(planets)
-        })()
+            let planetarr = await getPlanets()
+            console.log(planetarr)
+            setPlanet(planetarr)
+        })(planets)
 
 
 
@@ -20,8 +21,15 @@ let Part2 = (props) => {
         <div className='container '>
             <Link className='btn btn-primary' to="/part1">Part I</Link>
             <div className='row fullPage justify-content-around align-items-center '>
-                <div className='col-auto'>
-                </div>
+                {
+                    planets.map((currentValue, index) => {
+                        return <div 
+                        key={index} 
+                        style={{ height: `${currentValue.population/planets.}%` }} 
+                        className='col bg-secondary' />
+                    })
+                }
+
 
 
             </div>
